3c9ed11a479446cc8b8e75f218bd48b2fb7d8bf9
Levente <valentin.levente2003@gmail.com>
2022-09-20 09:28:19 +0200

    pont kesz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //-------------- Pontok -------------//
+        Pont p1 = new Pont();
+        System.out.println(p1);
+
+        Pont p2 = new Pont(4,6);
+        System.out.println(p2);
+
+        Pont p3 = new Pont(10);
+        System.out.println(p3);
+        System.out.println(p3.getDistanceFromOrigo());
+
+        Pont p4 = new Pont(8);
+        Pont p5 = new Pont(8);
+        System.out.println(p4.getDistanceBeetwenPoints(p5));
+
+        Pont p6 = new Pont(12);
+        System.out.println(p6);
+        //-------------- Pontok -------------//
+
     }
 }
--- src/Pont.java
+++ src/Pont.java
@@ -1,2 +1,56 @@
+import java.lang.invoke.SwitchPoint;
+import java.util.Random;
+
 public class Pont {
+    private int x;
+    private int y;
+
+    public Pont(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Pont(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    Random rnd = new Random();
+    public Pont(int n){
+        this.x = rnd.nextInt(n * 2) - n;
+        this.y = rnd.nextInt(n * 2) - n;
+    }
+
+    public double getDistanceFromOrigo(){
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+    public double getDistanceBeetwenPoints(Pont p){
+        return Math.sqrt(Math.pow(p.x - this.x, 2) + Math.pow(p.y - this.y, 2));
+    }
+
+    public int getQuadron(){
+
+        if(this.x < 0 && this.y > 0){
+            return 1;
+        }
+        else if(this.x < 0 && this.y < 0){
+            return 2;
+        }
+        else if(this.x > 0 && this.y < 0){
+            return 3;
+        }
+        else if(this.x > 0 && this.y > 0){
+            return 4;
+        }
+
+        return 1;
+    }
+
+
+    public String toString (){
+        return("X: " + this.x + " Y: " + this.y);
+    }
+
+
 }
